f270e4413a9c4072e2c976765a9c1b16232d9934
Luiz-Gusta <luizgustavocorreia2@gmail.com>
2023-11-06 21:28:38 -0300

    gradient, buttons and font

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import Button from "./components/Button";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
 
 import styles from "./styles";
 import Header from "./components/Header";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'BebasNeue': require('../assets/fonts/BebasNeue.ttf'),
+  });
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <>
       <Header />
@@ -16,15 +36,23 @@ export default function App() {
         end={LinearGradientPoint = { x: 0.0, y: 1.0 }}
       >
         <View style={styles.splitView}>
+          
           <View style={styles.leftContainer}>
-            <Text>SISTEMA DIGITAL DE PROTEÇÃO AO CIDADÃO</Text>
+
+            <View style={styles.content}>
+            <Text style={{fontSize: 60, fontFamily: 'BebasNeue', color: '#002756'}}>SISTEMA DIGITAL DE PROTEÇÃO AO CIDADÃO</Text>
             <View style={styles.buttons}>
               <Button value={'fazer ligação'} onPress={() => console.log('aaa')} />
               <Button value={'Emergência'} onPress={() => console.log('aaa')} />
             </View>
+            </View>
           </View>
-          <View style={styles.rightContainer}>
 
+          <View style={styles.rightContainer}>
+            <Text>SISTEMA DIGITAL DE PROTEÇÃO AO CIDADÃO</Text>
+            <View style={styles.buttons}>
+              <Button value={'fazer ligação'} onPress={() => console.log('aaa')} />
+            </View>
           </View>
         </View>
       </LinearGradient>
--- src/styles.js
+++ src/styles.js
@@ -1,23 +1,59 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({
+export default StyleSheet.create(
+  
+  {
     container: {
       flex: 1,
       backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    background: {
+      zIndex: 0,
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0
     },
 
     splitView:{
+      flex: 1,
       flexDirection: 'row',
-      paddingHorizontal: 50,
+      padding: 50,
     },
 
     leftContainer:{
+      flex: 1.5,
+    },
+
+    content:{
+     flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+
+    buttons:{
+      height: 100,
+      flexDirection: 'row',
+      gap: 40,
+    },
+
+    rightContainer:{
+      flex: 1,
       flexDirection: 'column',
       alignContent: 'space-around',
-      gap: 20,
+      backgroundColor: 'blue'
 
     },
 
-
+    title: {
+      //fontFamily: 'BebasNeue',
+      fontSize: 50,
+      fontWeight: 'bold',
+      color: '#002756'
+    }
 
   });
